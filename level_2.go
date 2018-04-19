6c039b198cfa461eb8d033710a0b592095f624b8
Ozan Kasikci <kasikciozan@yandex.com>
2018-04-19 20:24:53 +0300

    level2: comment out for now

--- level_2.go
+++ level_2.go
@@ -1,27 +1,27 @@
 package fantasia
 
-import "github.com/nsf/termbox-go"
+//import "github.com/nsf/termbox-go"
 
-type Level2 struct {
-	*Level
-	Stage *Stage
-}
+//type Level2 struct {
+//	*Level
+//	Stage *Stage
+//}
 
-func NewLevel2() *Level {
-	user := NewUser()
-	cells := []*TileMapCell{
-		{&termbox.Cell{'▓', termbox.ColorGreen, bgColor}, false},
-	}
-
-	e := NewEntity(20, 20, 1, 1, ' ', termbox.ColorBlue, termbox.ColorWhite, cells)
-	u := &User{
-		Entity: e,
-		Name:   "Test",
-	}
-	var entities []Renderer
-	entities = append(entities, user, u)
-
-	return &Level{
-		Entities: entities,
-	}
-}
+//func NewLevel2() *Level {
+//	user := NewUser()
+//	cells := []*TileMapCell{
+//		{&termbox.Cell{'▓', termbox.ColorGreen, bgColor}, false},
+//	}
+//
+//	e := NewEntity(20, 20, 1, 1, ' ', termbox.ColorBlue, termbox.ColorWhite, cells)
+//	u := &User{
+//		Entity: e,
+//		Name:   "Test",
+//	}
+//	var entities []Renderer
+//	entities = append(entities, user, u)
+//
+//	return &Level{
+//		Entities: entities,
+//	}
+//}
